f74bd2f09f8564e3ada3d705810a9bf3aa718965
Kareem Mostafa Hassaan <developer@ninjacod3r.com>
2016-06-30 18:35:29 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 require_once("BulkSMS.php");
 
 
-$obj = new BulkSMS('E7GEZLY', 'BSMS','http://smsbulk.eg.mobizone.mobi/BSMS/BSendAPI?');
+$obj = new BulkSMS('', '','');
 
 //message body , language , recipient , sender
 $obj->sendSMS('The reservation confirm ', 'en' , 201281264677 ,'E7gezly');
